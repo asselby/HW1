cf67d02769b3d16075364febffa0fac2f9b67f8d
asselby <asselid@bk.ru>
2018-04-23 16:32:04 +0600

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string message = Console.ReadLine();
+
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 4455);
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(ipEndPoint);
+
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+
+            int bytesSent = sender.Send(msg);
+
+            byte[] buffer = new byte[1024];
+            int bytesRec = sender.Receive(buffer);
+            string response = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine(response);
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 4455);
+
+            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(endPoint);
+            listener.Listen(5);
+
+            Socket socket = listener.Accept();
+
+            byte[] byffer = new byte[1024];
+            socket.Receive(byffer);
+
+            string data = Encoding.UTF8.GetString(byffer, 0, byffer.Length);
+
+            Console.Write(data);
+
+            byte[] msg = Encoding.UTF8.GetBytes("Response! " + data);
+            socket.Send(msg);
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            Console.Read();
+        }
+    }
+}
